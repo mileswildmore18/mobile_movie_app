9ab06059c2548fa566d49179b7e6f7ab3c79cc08
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-19 15:38:41 -0400

    Added the API from the TMDB website to fecth movies from their website to generate the search for the users to find movies based on their request or the query based on their request.

---
+++ services/api.ts
@@ -0,0 +1,30 @@
+// import API Key to get movies
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+
+export const fetchMovies = async ({query}: { query: string }) => {
+    // get movie data based on user's searched movie or based on the query
+    const endpoint = query
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+//     get movie search results
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+    // check if response fails
+    if (!response.ok) {
+        throw new Error('Failed to fetch movies', response.statusText);
+    }
+    const data = await response.json();
+
+    return data.results;
+}
+// /discover/movie
