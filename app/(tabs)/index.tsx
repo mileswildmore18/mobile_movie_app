12c6c1fc7a1580f4541043bcf98ab2ed5ac2a77e
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-19 14:42:09 -0400

    Added a background color and added the logo of the website at the top. Made the scrolling responsive for a user using their phone to make it scroll up and down.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
-import {Text, View} from "react-native";
+import {Image, ScrollView, Text, View} from "react-native";
 import {Link} from "expo-router";
+import {images} from "@/constants/images";
+import {icons} from "@/constants/icons";
 
 export default function Index() {
     return (
-        <View className="flex-1 justify-center items-center">
-            <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
-        </View>
+       //  Add background to home page
+       <View className="flex-1 bg-primary">
+    {/* Display pictures of movies*/}
+    <Image source={images.bg} className="absolute w-full z-0 "/>
+           {/*Make screen scrollable*/}
+           <ScrollView className="flex-1 px-5"
+                       showsHorizontalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
+            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+           </ScrollView>
+       </View>
     );
 }
