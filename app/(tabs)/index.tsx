1a855624458137dea36eba37bd1af6ea704a9b09
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-19 14:51:07 -0400

    Added a new component that adds the search bar into the website and make it activate the text box for the mobile phone to type in what users want to search.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import {Image, ScrollView, Text, View} from "react-native";
 import {Link} from "expo-router";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 export default function Index() {
     return (
@@ -13,6 +14,10 @@ export default function Index() {
            <ScrollView className="flex-1 px-5"
                        showsHorizontalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+               <View className="flex-1 mt-5">
+                   <SearchBar />
+               </View>
            </ScrollView>
        </View>
     );
