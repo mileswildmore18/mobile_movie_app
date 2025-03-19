8451e64d6bf6f84f29517a9183cf1bc77a155b29
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-19 14:20:43 -0400

    Changed the border of the icons along with getting them to naivgate to another page when users tap the icons on their phones along with leaving words on non focused icons blank and the focused items with the name of the navigation section of the website.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -12,7 +12,7 @@ const TabIcon = ({focused, icon, title}: any) => {
         return (
             <ImageBackground
                 source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
                 <Image source={icon} tintColor="#151312" className="size-5"/>
                 <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
             </ImageBackground>
@@ -28,7 +28,28 @@ const TabIcon = ({focused, icon, title}: any) => {
 }
 const _Layout = () => {
     return (
-        <Tabs>
+        <Tabs
+            /* Remove label from tabs*/
+            screenOptions={{tabBarShowLabel: false,
+            tabBarItemStyle: {
+            width:'100%',
+            height:'100%',
+            justifyContent:'center',
+            alignItems:'center'
+            },
+            //  Add border around the tabs and position the tabs
+            tabBarStyle: {
+                backgroundColor: '#0f0D23',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: "absolute",
+                overflow: "hidden",
+                borderWidth: 1,
+                borderColor: "#0f0D23"
+            }
+            }}>
             {/* Hide current tab header*/}
             {/* Add Home Tab*/}
             <Tabs.Screen
