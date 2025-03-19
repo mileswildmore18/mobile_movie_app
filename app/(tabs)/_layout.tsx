ba684e60c4efacef2cde394cfbe509553b1974cf
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-19 14:05:21 -0400

    Added the property of when the icons are focused and which ones are not and made them aligned neatly.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,43 +5,53 @@ import {Tabs} from "expo-router";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 
-const TabIcon = ({ focused, icon, title } : any) => {
+// Tab Icon
+const TabIcon = ({focused, icon, title}: any) => {
+    // Display focused icons
+    if (focused) {
+        return (
+            <ImageBackground
+                source={images.highlight}
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
+                <Image source={icon} tintColor="#151312" className="size-5"/>
+                <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
+            </ImageBackground>
+        )
+    }
+
+    // Display unfocused icons if not on the focused tab
     return (
-        <ImageBackground
-            source={images.highlight}
-            className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
-            <Image source={icon} tintColor="#151312" className="size-5" />
-            <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
-        </ImageBackground>
+        <View className="size-full justify-center items-center mt-4 rounded-full">
+            <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+        </View>
     )
 }
-
 const _Layout = () => {
     return (
         <Tabs>
-           {/* Hide current tab header*/}
-           {/* Add Home Tab*/}
-           <Tabs.Screen
-           name="index"
-           options={{
-               title: "Home",
-               headerShown: false,
-               tabBarIcon: ({ focused }) => (
-                   <TabIcon
-                       focused={focused}
-                       icon={icons.home}
-                       title="Home"
-                   />
-               )
-           }}
-           />
+            {/* Hide current tab header*/}
+            {/* Add Home Tab*/}
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.home}
+                            title="Home"
+                        />
+                    )
+                }}
+            />
             {/* Add Search Tab*/}
             <Tabs.Screen
                 name="search"
                 options={{
                     title: "Search",
                     headerShown: false,
-                    tabBarIcon: ({ focused }) => (
+                    tabBarIcon: ({focused}) => (
                         <TabIcon
                             focused={focused}
                             icon={icons.search}
@@ -57,7 +67,7 @@ const _Layout = () => {
                 options={{
                     title: "Saved",
                     headerShown: false,
-                    tabBarIcon: ({ focused }) => (
+                    tabBarIcon: ({focused}) => (
                         <TabIcon
                             focused={focused}
                             icon={icons.save}
@@ -73,7 +83,7 @@ const _Layout = () => {
                 options={{
                     title: "Profile",
                     headerShown: false,
-                    tabBarIcon: ({ focused }) => (
+                    tabBarIcon: ({focused}) => (
                         <TabIcon
                             focused={focused}
                             icon={icons.person}
