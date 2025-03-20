206ab895a6d44f21b6eb39e74d3af51c6edafb5f
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-20 13:04:12 -0400

    Added the tailwind properties to display the posters of the movies. Also added the pathway to see the movie posters for users to see when they open the website.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -4,9 +4,10 @@ import {Link} from "expo-router";
 
 // Add movie card properties
 const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) => {
+    console.log(poster_path);
     return (
         // Link to movie details
-        <Link href={`/movie/${id}`} asChild>
+        <Link href={`/movies/${id}`} asChild>
             {/* Display movie poster and make it clickable*/}
             <TouchableOpacity className="w-[30%]">
                 {/* Display movie poster*/}
