dafc4f61b08debaa1d7639967d255e62556f2835
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-20 13:17:38 -0400

    Added the image of star ratings for each movie displayed along with how many stars out of 5 the movies are.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import {View, Text, TouchableOpacity, Image} from 'react-native'
 import React from 'react'
 import {Link} from "expo-router";
+import {icons} from "@/constants/icons";
 
 // Add movie card properties
 const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) => {
@@ -15,13 +16,19 @@ const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) 
                     source={{
                         uri: poster_path
                         ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-                        : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
+                        : "https://placehold.co/600x400/1a1a1a/FFFFFF.png"
                     }}
                     className="w-full h-52 rounded-lg"
                     resizeMode="cover"
                     />
 
-                <Text className="text-sm font-bold text-white">{title}</Text>
+                <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+
+                <View className="flex-row items-center justify-start gap-x-1">
+                <Image source={icons.star} className="size-4"/>
+
+                    <Text className="text-xs text-white font-bold uppercase">{Math.round(vote_average / 2)}</Text>
+                </View>
             </TouchableOpacity>
         </Link>
     )
