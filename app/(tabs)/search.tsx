d4aecb09af9f95ae142e6e0a3b7e15ead19b1897
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-21 10:50:40 -0400

    Added the search results for a movie that users get when they type in the movie they are looking for as well as adding an error messge if search results go wrong. Next will be to add the query result for the movie users are looking for.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,12 @@
 // // Add search page for mobile
-import {FlatList, Image, Text, View} from 'react-native'
+import {ActivityIndicator, FlatList, Image, Text, View} from 'react-native'
 import {images} from "@/constants/images";
 import MovieCard from "@/components/MovieCard";
 import {useRouter} from "expo-router";
 import useFetch from "@/services/useFetch";
 import {fetchMovies} from "@/services/api";
+import {icons} from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 const Search = () => {
 
@@ -14,21 +16,63 @@ const Search = () => {
     // Get data from API to get movie results
     const {
         data: movies,
-        loading: moviesLoading,
-        error: moviesError
+        loading,
+        error
     } = useFetch(() => fetchMovies({
         query: ""
     }))
     return (
         <View className="flex-1 bg-primary">
-            <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />
-            
+            <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover"/>
+
+            {/* Display movie cards in a grid*/}
             <FlatList
                 data={movies}
-                renderItem={({ item }) => <MovieCard {...item} />}
-            />
+                renderItem={({item}) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                /*Divide the elements into columns and spread them out evenly*/
+                className="px-5"
+                numColumns={3}
+                columnWrapperStyle={{
+                    justifyContent: 'center',
+                    gap: 16,
+                    marginVertical: 16
+                }}
+                contentContainerStyle={{paddingBottom: 100}}
 
+                ListHeaderComponent={
+                    <>
+                        {/* Add logo at header*/}
+                        <View className="w-full flex-row justify-center mt-20 items-center">
+                            <Image source={icons.logo} className="w-12 h-10"/>
+                        </View>
+                        {/* Add search bar*/}
+                        <View className="my-5">
+                            <SearchBar placeholder="Search movies..."/>
+                        </View>
+                        {/* Display loading movies indicator*/}
+                        {loading && (
+                            <ActivityIndicator size="large" color="#0000ff" className="my-3"/>
+                        )}
+                        {/* Display error is movie is not found or error loading page*/}
+                        {error && (
+                            <Text className="text-red-500 px-5 my-3">
+                                Error: {error.message}
+                            </Text>
+                        )}
+
+                        {
+                            !loading && !error && 'SEARCH TERM'.trim()
+                            && movies?.length > 0 && (
+                            <Text className="text-xl text-white font-bold">
+                                Search Results for{' '}
+                                <Text className="text-accent">SEARCH TERM</Text>
+                            </Text>
+                        )}
+                    </>
+                }
+            />
         </View>
-    )
+    );
 }
 export default Search
