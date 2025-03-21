bf2e0d66d982025c6aa40f81ae91fc18fa2301af
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-21 10:15:07 -0400

    Added the release date for the movies trending and what category it is in for later on.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -22,12 +22,22 @@ const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) 
                     resizeMode="cover"
                     />
 
-                <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+                <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>{title}</Text>
 
                 <View className="flex-row items-center justify-start gap-x-1">
                 <Image source={icons.star} className="size-4"/>
 
                     <Text className="text-xs text-white font-bold uppercase">{Math.round(vote_average / 2)}</Text>
+                </View>
+                {/* Display release date*/}
+                <View className="flex-row items-center justify-between">
+                    <Text className="text-xs text-light-300 font-medium mt-1">
+                        {release_date?.split('-')[0]}
+                    </Text>
+                    {/* Display category*/}
+                    {/*<Text className="text-xs font-medium text-light-300 uppercase">*/}
+                    {/*    Movie*/}
+                    {/*</Text>*/}
                 </View>
             </TouchableOpacity>
         </Link>
