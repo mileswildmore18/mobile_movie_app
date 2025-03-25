bad210e34c3f11f62868a431bdd4b7e17d7f1c1b
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-25 15:03:39 -0400

    Added the Flatlist to show movies trending as of now. Also added the data searched for movies to be added cleanly into the appwrite. Next will be to add posters of movies in the trending movies section.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -67,6 +67,20 @@ export default function Index() {
                         )}
 
                         <>
+
+                            {/*Display trending movies*/}
+                            <FlatList
+                                horizontal
+                                showsHorizontalScrollIndicator={false}
+                                ItemSeparatorComponent={() => <View className="w-4"/>}
+                                className="mb-4 mt-3"
+                                data={trendingMovies}
+                                renderItem={({item, index})=>(
+                                <Text className="text-white text-sm">{item.title}</Text>
+                            )}
+                                keyExtractor={(item) => item.movie_id.toString()}
+                                      />
+
                             <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
                             {/* Display movies organized into columns*/}
                             <FlatList
