b135f720529596614138509dbbc656c34b734a8c
Miles Wildmore <mileswildmore18@gmail.com>
2025-03-26 12:10:41 -0400

    Added the saved and profile pages with the background similar to the other pages.

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,11 +1,19 @@
 // Add saved movies page for mobile
-import { Text, View} from 'react-native'
+import {Image, Text, View} from 'react-native'
 import React from 'react'
+import {icons} from "@/constants/icons";
 
 const Saved = () => {
     return (
-        <View>
-            <Text>Saved</Text>
+        <View className="bg-primary flex-1 px-10">
+            <View className="flex justify-center items-center flex-1 flex-col gap-5">
+                <Image
+                    source={icons.save}
+                    className="size-10"
+                    tintColor="#FFF"
+                />
+                <Text className="text-gray-500 text-base">Save</Text>
+            </View>
         </View>
     )
 }
